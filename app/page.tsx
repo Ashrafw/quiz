005a39301d981c45742357bea74544e9672cdd81
Ashraf <ashraf@Ashrafs-MacBook-Pro.local>
2023-06-29 06:19:36 +0100

    logos

--- app/page.tsx
+++ app/page.tsx
@@ -13,17 +13,31 @@ export default function Home() {
     router.push("/quiz");
     // console.log("hello");
   };
+
   return (
     <main className="flex  flex-col items-center justify-between p-2">
       <p className="py-4 px-2 text-center text-[18px]">
-        Do you have what it takes to become the react quiz master?
+        Do you have what it takes to become the quiz master?
       </p>
-      <div className="flex gap-2">
-        <Image src={react} alt="logo" className="w-[160px] mt-2 border-4 rounded-full cursor-pointer" />
-        <Image src={javascript} alt="logo" className="w-[160px] mt-2 border-4 rounded-full" />
-        <Image src={python} alt="logo" className="w-[160px] mt-2 border-4 rounded-full" />
+      <p className="py-4 px-2 text-center text-[18px]">Choose your language</p>
+      <div className="flex gap-6 flex-col">
+        <Image
+          src={react}
+          alt="logo"
+          className="w-[240px] mt-2 border-[6px] rounded-full cursor-pointer hover:border-blue-700"
+        />
+        <Image
+          src={javascript}
+          alt="logo"
+          className="w-[240px] mt-2 border-[6px] rounded-full cursor-pointer hover:border-yellow-400"
+        />
+        <Image
+          src={python}
+          alt="logo"
+          className="w-[240px] mt-2 border-[6px] rounded-full cursor-pointer hover:border-green-500"
+        />
       </div>
-      <Button text="Start Quiz" onClick={handleButtonClick} />
+      {/* <Button text="Start Quiz" onClick={handleButtonClick} /> */}
     </main>
   );
 }
