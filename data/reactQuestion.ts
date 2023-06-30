d4f95dc96719a56c95e00627c4fd6be0a82894aa
Ashraf <ashraf@Ashrafs-MacBook-Pro.local>
2023-06-30 21:18:54 +0100

    pages

---
+++ data/reactQuestion.ts
@@ -0,0 +1,188 @@
+export const reactQuestions = [
+  {
+    question: "What is the main purpose of React Router?",
+    options: [
+      "To handle routing in React applications.",
+      "To manage state in React applications.",
+      "To style React components.",
+      "To handle data fetching in React applications.",
+    ],
+    correctAnswer: "To handle routing in React applications.",
+  },
+  {
+    question: "What is the significance of keys in React lists?",
+    options: [
+      "Keys help identify elements uniquely in lists.",
+      "Keys determine the order of elements in lists.",
+      "Keys define the styling of list items.",
+      "Keys manage the state of list components.",
+    ],
+    correctAnswer: "Keys help identify elements uniquely in lists.",
+  },
+  {
+    question: "What is the purpose of React Fragments?",
+    options: [
+      "To group multiple elements without adding extra nodes to the DOM.",
+      "To render complex HTML structures in React components.",
+      "To enable two-way data binding in React components.",
+      "To define conditional rendering in React components.",
+    ],
+    correctAnswer: "To group multiple elements without adding extra nodes to the DOM.",
+  },
+  {
+    question: "Which method is used to change the state of a React component?",
+    options: ["setState()", "changeState()", "updateState()", "modifyState()"],
+    correctAnswer: "setState()",
+  },
+  {
+    question: "What is the purpose of PropTypes in React?",
+    options: [
+      "To validate the types of props passed to components.",
+      "To style React components using CSS-in-JS.",
+      "To handle asynchronous operations in React components.",
+      "To manage the state of functional components.",
+    ],
+    correctAnswer: "To validate the types of props passed to components.",
+  },
+  {
+    question: "What is the purpose of React DevTools?",
+    options: [
+      "To inspect and debug React component hierarchy.",
+      "To optimize performance in React applications.",
+      "To create responsive layouts in React applications.",
+      "To handle form validation in React components.",
+    ],
+    correctAnswer: "To inspect and debug React component hierarchy.",
+  },
+  {
+    question: "What is the purpose of the shouldComponentUpdate() method?",
+    options: [
+      "To control whether a component should re-render or not.",
+      "To fetch data from an API in React components.",
+      "To handle user input in React components.",
+      "To define the initial state of a React component.",
+    ],
+    correctAnswer: "To control whether a component should re-render or not.",
+  },
+  {
+    question: "What are React hooks?",
+    options: [
+      "Functions that let you use React features in functional components.",
+      "Predefined CSS classes provided by React for styling components.",
+      "Methods used to create custom React components.",
+      "Data structures used to manage state in React applications.",
+    ],
+    correctAnswer: "Functions that let you use React features in functional components.",
+  },
+  {
+    question: "What is the purpose of the useEffect() hook in React?",
+    options: [
+      "To perform side effects in functional components.",
+      "To handle user interactions in React components.",
+      "To define the initial state of a functional component.",
+      "To render JSX elements in functional components.",
+    ],
+    correctAnswer: "To perform side effects in functional components.",
+  },
+  {
+    question: "What is the difference between controlled and uncontrolled components in React forms?",
+    options: [
+      "Controlled components manage form state internally, while uncontrolled components rely on the DOM.",
+      "Controlled components use class components, while uncontrolled components use functional components.",
+      "Controlled components are used for controlled data fetching, while uncontrolled components are used for uncontrolled data fetching.",
+      "Controlled components handle form validation, while uncontrolled components handle form submission.",
+    ],
+    correctAnswer:
+      "Controlled components manage form state internally, while uncontrolled components rely on the DOM.",
+  },
+  {
+    question: "What is the purpose of the useContext() hook in React?",
+    options: [
+      "To access the value of a context in functional components.",
+      "To manage routing in React applications.",
+      "To handle AJAX requests in React components.",
+      "To create reusable component logic in React.",
+    ],
+    correctAnswer: "To access the value of a context in functional components.",
+  },
+  {
+    question: "What is the role of a reducer in React's useReducer() hook?",
+    options: [
+      "To specify how the state should change in response to dispatched actions.",
+      "To optimize the performance of React components.",
+      "To handle form submission in React components.",
+      "To define the initial state of a React component.",
+    ],
+    correctAnswer: "To specify how the state should change in response to dispatched actions.",
+  },
+  {
+    question: "What is the purpose of the useCallback() hook in React?",
+    options: [
+      "To memoize functions and prevent unnecessary re-renders.",
+      "To handle user input in React components.",
+      "To define the initial state of a functional component.",
+      "To perform side effects in functional components.",
+    ],
+    correctAnswer: "To memoize functions and prevent unnecessary re-renders.",
+  },
+  {
+    question: "What is the role of the useMemo() hook in React?",
+    options: [
+      "To memoize a value and prevent unnecessary re-computations.",
+      "To handle AJAX requests in React components.",
+      "To create reusable component logic in React.",
+      "To define the initial state of a functional component.",
+    ],
+    correctAnswer: "To memoize a value and prevent unnecessary re-computations.",
+  },
+  {
+    question: "What is the purpose of React's forwardRef() function?",
+    options: [
+      "To forward a ref from a parent component to a child component.",
+      "To manage state in class components.",
+      "To handle form validation in React components.",
+      "To create a higher-order component in React.",
+    ],
+    correctAnswer: "To forward a ref from a parent component to a child component.",
+  },
+  {
+    question: "What is the purpose of the React context API?",
+    options: [
+      "To share state between multiple components without passing props manually at each level.",
+      "To handle routing in React applications.",
+      "To manage form submission in React components.",
+      "To optimize performance in React applications.",
+    ],
+    correctAnswer: "To share state between multiple components without passing props manually at each level.",
+  },
+  {
+    question: "What is React's virtual DOM?",
+    options: [
+      "A lightweight copy of the actual DOM used for efficient rendering and updates.",
+      "A virtual machine for running React applications.",
+      "A separate thread for handling I/O operations in React.",
+      "A package manager for React applications.",
+    ],
+    correctAnswer: "A lightweight copy of the actual DOM used for efficient rendering and updates.",
+  },
+  {
+    question: "What is the purpose of React's prop drilling?",
+    options: [
+      "To pass data from a parent component to nested child components through multiple levels.",
+      "To handle AJAX requests in React components.",
+      "To create reusable component logic in React.",
+      "To optimize the performance of React components.",
+    ],
+    correctAnswer: "To pass data from a parent component to nested child components through multiple levels.",
+  },
+  {
+    question: "What is the role of the PureComponent class in React?",
+    options: [
+      "To provide automatic performance optimizations for React components.",
+      "To handle form submission in React components.",
+      "To define the initial state of a React component.",
+      "To manage routing in React applications.",
+    ],
+    correctAnswer: "To provide automatic performance optimizations for React components.",
+  },
+];
